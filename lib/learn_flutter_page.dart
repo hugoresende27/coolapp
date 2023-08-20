aa3436a365f1c1b0e2bc0e12b1d83a7861ea388b
hugoresende <hugo.resende@iservices.pt>
2023-08-20 12:00:29 +0100

    learn flutter page

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -11,21 +11,48 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Learn Flutter Hugo'),
-        automaticallyImplyLeading: false,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.of(context).pop();  //to delete the page (go to the previous page, back button)
-          },
-          icon: const Icon(Icons.arrow_back_ios),
+        appBar: AppBar(
+          title: const Text('Learn Flutter Hugo'),
+          automaticallyImplyLeading: false,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.of(context)
+                  .pop(); //to delete the page (go to the previous page, back button)
+            },
+            icon: const Icon(Icons.arrow_back_ios),
+          ),
         ),
-      ),
-      body: Column(
-        children: [
-          Image.asset('images/image1.jpg')
-        ],
-      )
-    );
+        body: Column(
+          children: [
+            Image.asset('images/image1.jpg'),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(
+              color: Colors.black,
+            ),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.blueGrey,
+              width:
+                  double.infinity, //double.infinity to get max width possible
+              child: const Center(
+                child: Text(
+                  'This is a text widget',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                debugPrint('Elevated button here');
+              },
+              child: const Text('Elevated Button'),
+            ),
+          ],
+        ));
   }
 }
